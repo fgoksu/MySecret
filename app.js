e7087b9b90364b57d152206e74a4ddbd67e092fc
Furkan Goksu <47495420+fgoksu@users.noreply.github.com>
2019-12-26 11:28:19 +0100

    Add Enviroment Vars

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 //jshint esversion:6
+require('dotenv').config()
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require('mongoose-encryption');
 
 const app = express();
+
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -14,13 +19,15 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true, useUnifiedTopology:true});
 
-const userSchema ={
+const userSchema = new mongoose.Schema ({
     email: String,
     password: String
-};
+});
 
-const User = new mongoose.model("User",userSchema);
 
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+ 
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req,res){
     res.render("home");
@@ -33,6 +40,8 @@ app.get("/login", function(req,res){
 app.get("/register", function(req,res){
     res.render("register");
 });
+
+
 
 app.post("/register",function(req,res){
     const newUser = new User({
@@ -65,8 +74,6 @@ app.post("/login",function(req,res){
     });
 
 });
-
-
 
 
 app.listen(3000,function(){
